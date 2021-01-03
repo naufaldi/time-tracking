4882c448ee2b4688136fabac80849f3e6d7c9f21
naufaldi <naufaldi.rafif@gmail.com>
2021-01-03 21:38:56 +0700

    finish dekstop and responsive

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,94 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Time Tracker</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+      <header id="hero" className="lg:h-screen w-full bg-gray-500">
+        <nav className="max-w-screen-xl mx-auto px-4 grid grid-cols-6 md:grid-cols-12 gap-4 pt-10 items-center">
+          <div className="col-span-2 ">
+            <img
+              src="/assets/img/Logo.png"
+              className="mx-4"
+              alt="Logo Time Tracking"
+            />
+          </div>
+          <ul className="col-start-5 md:col-start-5 lg:col-start-7 xl:col-start-8 col-span-2  md:col-span-8 lg:col-span-6 xl:col-span-5 flex justify-end text-xl text-dark-200 font-medium">
+            <li className="hidden md:block cursor-pointer hover:underline transition-all mx-4 px-2 ">
+              Feature
+            </li>
+            <li className="hidden md:block cursor-pointer hover:underline transition-all mx-4 px-2 ">
+              Pricing
+            </li>
+            <li className="hidden md:block cursor-pointer hover:underline transition-all mx-4 px-2 ">
+              Why Gravity
+            </li>
+            <li className="hidden md:block cursor-pointer hover:underline transition-all mx-4 px-2 text-acapulco-500">
+              Sign In
+            </li>
+            <li className="block md:hidden w-full">
+              <button className="border-2 border-black shadow-sm py-2 px-4 rounded-lg flex items-center tex-xs">
+                <svg
+                  className="mr-4"
+                  width="12"
+                  height="12"
+                  viewBox="0 0 12 12"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M11.2674 1.77074C11.2674 2.74869 10.4711 3.54147 9.48879 3.54147C8.5065 3.54147 7.71021 2.74869 7.71021 1.77074C7.71021 0.792786 8.5065 -2.14208e-08 9.48879 -4.78447e-08C10.4711 -7.42686e-08 11.2674 0.792786 11.2674 1.77074Z"
+                    fill="#22322F"
+                  />
+                  <path
+                    d="M11.2712 9.45238C11.2712 10.4303 10.4749 11.2231 9.49257 11.2231C8.51029 11.2231 7.71399 10.4303 7.71399 9.45238C7.71399 8.47443 8.51029 7.68164 9.49257 7.68164C10.4749 7.68164 11.2712 8.47443 11.2712 9.45238Z"
+                    fill="#22322F"
+                  />
+                  <path
+                    d="M3.55717 1.77074C3.55717 2.74869 2.76087 3.54147 1.77858 3.54147C0.796299 3.54147 7.39408e-08 2.74869 4.76336e-08 1.77074C2.13263e-08 0.792786 0.796299 -2.14208e-08 1.77858 -4.78447e-08C2.76087 -7.42686e-08 3.55717 0.792786 3.55717 1.77074Z"
+                    fill="#22322F"
+                  />
+                  <path
+                    d="M3.56169 9.44505C3.56169 10.423 2.76539 11.2158 1.7831 11.2158C0.800816 11.2158 0.00451668 10.423 0.00451665 9.44505C0.00451662 8.4671 0.800816 7.67432 1.7831 7.67432C2.76539 7.67432 3.56169 8.4671 3.56169 9.44505Z"
+                    fill="#22322F"
+                  />
+                </svg>
+                Menu
+              </button>
+            </li>
+          </ul>
+        </nav>
+        <div className="grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 gap-4 mt-12 md:mt-20   max-w-screen-xl mx-auto px-4 lg:items-center pb-24 md:pb-0">
+          <div className="w-full md:w-10/12  lg:w-full mx-auto flex flex-col justify-start items-start text-dark-500 mt-12 lg:mt-0 px-4 lg:px-0">
+            <h1 className="text-2xl md:text-4xl lg:text-5xl leading-relaxed">
+              Simple time tracking.
+              <br /> Powerful reporting.
+            </h1>
+            <p className="my-6 text-base md:text-xl lg:text-2xl">
+              Turn your team on to productivity with Gravity the time tracker.
             </p>
-          </a>
+            <form className="flex flex-col md:flex-row w-full items-start md:items-center">
+              <input
+                type="text"
+                class="text-dark-500 bg-transparent border-2 w-full md:w-7/12 lg:w-5/12 border-black rounded-lg shadow-sm py-3 px-4 md:text-lg"
+              />
+              <button className="md:text-lg font-bold border-2 border-black rounded-lg shadow py-3 px-4 bg-acapulco-500 text-white mt-4 md:mt-0 md:mx-4 hover:bg-acapulco-600 hover:border-acapulco-500">
+                Request early access
+              </button>
+            </form>
+          </div>
+          <div className="flex items-center justify-center row-start-1 lg:col-start-2">
+            <img
+              src="/assets/img/Illustration.png"
+              alt="Illustration Time Tracking"
+              className="w-full md:w-9/12 lg:w-10/12"
+            />
+          </div>
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      </header>
+    </>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: ["./pages/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    boxShadow: {
+      sm: "3px 2px 0px 0px hsl(0,0%,20%);",
+      DEFAULT: "6px 5px 0px 0px hsl(0,0%,20%);",
+    },
+    extend: {
+      colors: {
+        acapulco: {
+          500: "hsl(169,24%,55%)",
+          600: "hsl(175,24%,55%)",
+          700: "hsl(180,24%,55%)",
+        },
+        gray: {
+          500: "hsl(0,0%,98%)",
+        },
+        dark: {
+          200: "hsl(240,1%,19%)",
+          500: "hsl(169,19%,16%)",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
